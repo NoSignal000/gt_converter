c14c3c0f1a85ac67d9cae5091bf02c4da159d925
NoSignal000 <umeshprajapati20799@gmail.com>
2024-05-16 10:57:48 +0530

    16 may 11:00

--- file_converter/settings.py
+++ file_converter/settings.py
@@ -49,7 +49,6 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     # "http://localhost:8000",
-    # "https://d7f1-117-207-212-46.ngrok-free.app"
     "https://file-converter-0ndt.onrender.com",
 ]
 
